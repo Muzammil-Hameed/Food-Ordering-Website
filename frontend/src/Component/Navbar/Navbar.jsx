85b80f34c3246af7ca9548316a8af31bd4fd619b
Muzammil Hameed <muzhameed9547@gmail.com>
2025-07-24 16:00:12 +0500

    Final commit

--- frontend/src/Component/Navbar/Navbar.jsx
+++ frontend/src/Component/Navbar/Navbar.jsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/assets';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin }) => {
   const [menu, setMenu] = useState("menu");
+
+  const {getTotalCartAmount} = useContext(StoreContext);
 
   // Yeh useEffect window hash check karta hai aur state set karta hai
   useEffect(() => {
@@ -28,18 +32,18 @@ const Navbar = ({setShowLogin }) => {
 
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo' />
+      <Link to="/"><img src={assets.logo} alt="" className='logo' /></Link>
       <ul className='navbar-menu'>
-        <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
-        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>menu</a>
-        <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
-        <a href='#footer' onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : ""}>contact us</a>
+        <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+        <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : ""}>Mobile-App</a>
+        <a href='#footer' onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : ""}>Contact Us</a>
       </ul>
       <div className='navbar-right'>
         <img src={assets.search_icon} alt="" />
         <div className='navbar-search-icon'>
-          <img src={assets.basket_icon} alt="" />
-          <div className='dot'></div>
+          <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+          <div className={getTotalCartAmount()===0? "": "dot"}></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>sign in</button>
       </div>
